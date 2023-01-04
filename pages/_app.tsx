2808dfca485154c36dfd54aa45fcd8cd4ffab546
Leland Riordan <leland@eastrocklabs.com>
2023-01-04 10:52:16 -0500

    Add Vercel Analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,7 @@ import type { AppProps } from 'next/app'
 import { useEffect } from 'react'
 import { Karla } from '@next/font/google'
 import { GoogleAnalytics } from 'nextjs-google-analytics'
+import { Analytics } from '@vercel/analytics'
 
 const karla = Karla({
   subsets: ['latin'],
@@ -26,6 +27,7 @@ export default function App({ Component, pageProps }: AppProps) {
     <div className={`${karla.variable} font-sans`}>
       <GoogleAnalytics trackPageViews />
       <Component {...pageProps} />
+      <Analytics />
     </div>
   )
 }
